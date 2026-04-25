bc9905094c79d1e97ebcf13b552f66f2ed1269f4
skend-dev <skender.mustafa.96@gmail.com>
2026-04-25 12:08:06 +0200

    Refactor exception handling by moving AllExceptionsFilter to common filters directory
    - Updated import path for AllExceptionsFilter in main.ts.
    - Created new AllExceptionsFilter class in src/common/filters/all-exceptions.filter.ts to handle exceptions globally.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from "@nestjs/common";
-import { AllExceptionsFilter } from './users/CustomExeptionFilter';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
